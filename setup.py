73721cfa01aaa010a528a43d6fc26c17cc3cdeb7
张振 <zhangzhen@wondersgroup.com>
2020-06-21 21:54:14 +0800

    统一训练和推理流程，使用参考example

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ REQUIRED_PACKAGES = [
 ]
 setuptools.setup(
   name="litNlp",
-  version="0.6.12",
+  version="0.7.0",
   packages=['litNlp'],
   author="CarryChang",
   author_email="coolcahng@gmail.com",
