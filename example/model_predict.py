10eca86e9a1b9db4de43ee5529e1ca83b3045189
张振 <zhangzhen@wondersgroup.com>
2020-06-22 13:53:48 +0800

    add model structure

--- example/model_predict.py
+++ example/model_predict.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Time: 2020/6/21 0021 14:07
 from litNlp.predict import SA_Model_Predict
-# 内置参数，批处理文本
-predict_text = ['这个我不喜欢', '这个我喜欢不']
 # 初始化模型
-# 解决跨文件问题
-tokenize_path = 'sa_model/tokenizer.pickle'
-sa_model_path_m = 'sa_model/c_cnn_m.h5'
+tokenize_path = 'model/tokenizer.pickle'
+sa_model_path_m = 'model/model.h5'
+# e_comment
+predict_text = ['这个我不喜欢', '这个我喜欢不']
 model = SA_Model_Predict(tokenize_path, sa_model_path_m, max_len=100)
 sa_score = model.predict(predict_text)
 # 多分类模型输出
--- example/model_train.py
+++ example/model_train.py
@@ -2,7 +2,7 @@
 # @Time: 2020/6/21 0021 20:46
 import pandas as pd
 from litNlp.train import SA_Model_Train
-train_data = pd.read_csv('data/sa_data_train.csv')
+train_data = pd.read_csv('data/e_comment.csv')
 # data process
 train_data['text_cut'] = train_data['text'].apply(lambda x: " ".join(list(x)))
 # 最大句子长度
@@ -12,13 +12,13 @@ max_words = 1000
 # 设置embedding大小
 embedding_dim = 300
 # 模型的保存位置，后续用于推理
-sa_model_path_m = 'sa_model/c_cnn_m.h5'
+sa_model_path_m = 'model/model.h5'
 # 离线保存tokenizer
-tokenize_path ='sa_model/tokenizer.pickle'
+tokenize_path ='model/tokenizer.pickle'
 # 分类的类别数
 num_classes = 2
 label = train_data['label']
 train_data = train_data['text_cut']
 # train: evaluate默认在训练完毕之后开启计算
 model = SA_Model_Train(max_words, embedding_dim, maxlen, tokenize_path, sa_model_path_m)
-model.train(train_data, label, num_classes, batch_size=256, epochs=10, verbose=1, evaluate=True)
+model.train(train_data, label, num_classes, batch_size=256, epochs=2, verbose=1, evaluate=True)
